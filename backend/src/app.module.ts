02f45157b04c4582b362e4a0caf7dee644fa34d7
HM <hieumdd@gmail.com>
2022-06-04 17:39:11 +0700

    feat: Add database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+
+import { ConfigModule } from '@nestjs/config';
+import Joi from '@hapi/joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            validationSchema: Joi.object({
+                DB_HOST: Joi.string().required(),
+                DB_PORT: Joi.number().required(),
+                DB_USER: Joi.string().required(),
+                DB_PASSWORD: Joi.string().required(),
+                DB_DATABASE: Joi.string().required(),
+            }),
+        }),
+        DatabaseModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
