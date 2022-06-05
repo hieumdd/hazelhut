740ff5e1fcf875d2ac20a79f5772ceee17bcd88b
HM <hieumdd@gmail.com>
2022-06-05 09:59:26 +0700

    feat: Add price to Product

--- backend/src/models/product/entities/product.entity.ts
+++ backend/src/models/product/entities/product.entity.ts
@@ -1,13 +1,17 @@
-import { Column, Entity, ManyToOne, OneToMany } from 'typeorm';
+import { Check, Column, Entity, ManyToOne, OneToMany } from 'typeorm';
 
 import { Meta } from '@models/common/meta.entity';
 import { ProductCategory } from '@models/product-category/entities/product-category.entity';
 import { ProductAttributeValue } from '@models/product-attribute/entities/product-attribute-value.entity';
 
 @Entity()
+@Check('price > 0')
 export class Product extends Meta {
     @Column()
     name: string;
+
+    @Column()
+    price: number;
 
     @ManyToOne(() => ProductCategory, ({ products }) => products)
     category: number;
